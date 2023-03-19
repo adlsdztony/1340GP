6d38d3347c7095a7cc4585f08624c278b34a0f87
adlsdztony <zzl0712@outlook.com>
2023-03-19 12:46:07 +0800

    first commit

---
+++ keyboard.cpp
@@ -0,0 +1,32 @@
+#include <termio.h>
+#include <stdio.h>
+ 
+int scanKeyboard()
+{
+ 
+	int in;
+ 
+	struct termios new_settings;
+	struct termios stored_settings;
+    //设置终端参数
+	tcgetattr(0,&stored_settings);
+	new_settings = stored_settings;
+	new_settings.c_lflag &= (~ICANON);
+	new_settings.c_cc[VTIME] = 0;
+	tcgetattr(0,&stored_settings);
+	new_settings.c_cc[VMIN] = 1;
+	tcsetattr(0,TCSANOW,&new_settings);
+	in = getchar();
+	tcsetattr(0,TCSANOW,&stored_settings);
+ 
+	return in;
+ 
+}
+ 
+//测试函数
+int main(){
+ 
+	while(1){
+		printf("%d\n",scanKeyboard());
+	}
+}
